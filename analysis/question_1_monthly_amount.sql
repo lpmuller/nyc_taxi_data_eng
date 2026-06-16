f00e413810c0ca432e596300c4683ee1f58d51a5
Laura Muller <lp_muller@hotmail.com>
2026-06-16 00:25:53 -0300

    refactor: separate sql files

--- analysis/question_1_monthly_amount.sql
+++ analysis/question_1_monthly_amount.sql
@@ -7,14 +7,3 @@ SELECT
     total_trips
 FROM workspace.gold.yellow_taxi_monthly_amount
 ORDER BY pickup_month;
-
-
--- Question 2:
--- What is the average passenger count by hour of day?
-
-SELECT
-    pickup_hour,
-    avg_passenger_count,
-    valid_passenger_records
-FROM workspace.gold.yellow_taxi_hourly_passenger
-ORDER BY pickup_hour;
